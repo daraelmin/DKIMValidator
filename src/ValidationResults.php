4b66a5752c6dc2b3bac6b1ce009d538bc5623ad5
Marcus Bointon <marcus@synchromedia.co.uk>
2020-10-15 10:30:31 +0200

    Generate string and JSON output

--- src/ValidationResults.php
+++ src/ValidationResults.php
@@ -55,4 +55,80 @@ final class ValidationResults
     {
         return $this->valid;
     }
+
+    /**
+     * Generate nice text-based output.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        $out = "DKIM validation results\n";
+        $out .= "Overall status: " . ($this->isValid() ? 'pass' : 'fail') . "\n";
+        $signatureNumber = 1;
+        foreach ($this->results as $result) {
+            $out .= "Signature #" . $signatureNumber . "\n";
+            $out .= "Domain and selector: " . $result->getDomain() . '/' . $result->getSelector() . "\n";
+            $out .= "Validation result: " . ($result->isValid() ? 'pass' : 'fail') . "\n";
+            if (count($result->getFails()) > 0) {
+                $out .= "Validation failures:\n";
+                foreach ($result->getFails() as $fail) {
+                    $out .= $fail . "\n";
+                }
+            }
+            if (count($result->getWarnings()) > 0) {
+                $out .= "Validation warnings:\n";
+                foreach ($result->getWarnings() as $warning) {
+                    $out .= $warning . "\n";
+                }
+            }
+            if (count($result->getFails()) > 0) {
+                $out .= "Validation passes:\n";
+                foreach ($result->getPasses() as $pass) {
+                    $out .= $pass . "\n";
+                }
+            }
+            ++$signatureNumber;
+        }
+        return $out;
+    }
+
+    /**
+     * Generate JSON output.
+     *
+     * @return string
+     */
+    public function asJSON()
+    {
+        $out = [];
+        $out['valid'] = $this->isValid();
+        $out['signatures'] = [];
+        foreach ($this->results as $result) {
+            $resultSet = [];
+            $resultSet['valid'] = $result->isValid();
+            $resultSet['domain'] = $result->getDomain();
+            $resultSet['selector'] = $result->getSelector();
+            $resultSet['failures'] = [];
+            if (count($result->getFails()) > 0) {
+                foreach ($result->getFails() as $fail) {
+                    $resultSet['failures'][] = $fail;
+                }
+            }
+            $resultSet['warnings'] = [];
+            if (count($result->getWarnings()) > 0) {
+                foreach ($result->getWarnings() as $warning) {
+                    $resultSet['warnings'][] = $warning;
+                }
+            }
+            $resultSet['passes'] = [];
+            if (count($result->getFails()) > 0) {
+                foreach ($result->getPasses() as $pass) {
+                    $resultSet['passes'][] = $pass;
+                }
+            }
+            $out['signatures'][] = $resultSet;
+        }
+
+        return json_encode($out, JSON_PRETTY_PRINT);
+    }
 }
